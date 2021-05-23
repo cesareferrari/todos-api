fa13b409efd3efba4b2690571c823277a9f917de
Cesare Ferrari <cesare@maryandferrari.com>
2021-05-23 11:16:36 -0400

    Create specs for todos and items

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,4 @@
 class Todo < ApplicationRecord
+  validates :title, :created_by, presence: true
+  has_many :items, dependent: :destroy
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,7 +22,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -72,6 +72,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include RequestSpecHelper, type: :request
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
@@ -84,4 +85,5 @@ RSpec.configure do |config|
       example.run
     end
   end
+
 end
